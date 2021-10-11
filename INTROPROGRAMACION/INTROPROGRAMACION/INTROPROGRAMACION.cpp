054d182afaf06ec498a2af4bd95fb013bd10c879
ALANALEJANDRORODRIGUEZHERNANDEZ <alanrdzzhdzz@gmail.com>
2021-10-11 11:26:33 -0500

    Usando una funcion modificamos el programa para que sume 2 valores

--- INTROPROGRAMACION/INTROPROGRAMACION/INTROPROGRAMACION.cpp
+++ INTROPROGRAMACION/INTROPROGRAMACION/INTROPROGRAMACION.cpp
@@ -1,16 +1,21 @@
 #include <stdio.h>
-int main(){
+int sumadatos (int a, int b) {
+    int resultados;
+    resultados = a + b;
+    return resultados;
+}
 
+int main(){
     //Sumar dos numeros, pedirlos por teclado
     int num1, num2, resultado;
     printf("Programa que suma 2 numeros\n");
     //Lectura de los numeros
-    printf("Ingresa el primer numero: ");
+    printf("Teclea el primer numero: ");
     scanf_s("%i", &num1);
-    printf("Ingresa el primer numero: ");
+    printf("Teclea el segundo numero: ");
     scanf_s("%i", &num2);
     //Proceso
-    resultado = num1 + num2;
+    resultado = sumadatos(num1,num2);
     //Salida
     printf("El resultado es: %i ", resultado);
         return 0;
